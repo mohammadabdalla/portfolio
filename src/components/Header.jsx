01bcfb1b997cf343395244d79c9b61e4b6cb4b21
Mohammad <mohammadabdulla523@gmail.com>
2024-05-13 08:42:12 +0300

    deployment test 3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,7 +25,6 @@ const Header = ({ classicHeader, darkTheme, homeRef, handleNavClick }) => {
               <img
                 className="img-fluid rounded-pill d-block"
                 src={process.env.PUBLIC_URL +"/images/myphoto_small.jpg"}
-                alt="profile image"
               />
             </span>
             <h1 className="text-3 text-white text-center mb-0 d-lg-block">
