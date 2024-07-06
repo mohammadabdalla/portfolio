c2af628dae4f48746156a9a96e173ea8bfe89e0b
MOhammad <mohammadabdulla523@gmail.com>
2024-07-07 01:57:20 +0300

    email enabling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,13 +13,17 @@ import emailjs from '@emailjs/browser';
 
 
 const Contact = ({ data, socialData }) => {
-  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
-  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
-  const publicKey = process.env.REACT_APP_EMAILJS_PUBLICKEY;
+
+  
   const form = useRef();
 
   const sendEmail = (e) => {
+
     e.preventDefault();
+    
+    const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.REACT_APP_EMAILJS_PUBLICKEY;
 
     emailjs
       .sendForm(serviceId, templateId, form.current, {
